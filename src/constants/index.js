0888c238b50e12f42e47e4da509c8fc17fe18702
Swati Meher <52138974+swameher@users.noreply.github.com>
2026-04-22 11:15:37 +0530

    Revise constants for personal and professional details
    Updated the info and experiences

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,28 +4,35 @@ import project2 from "../assets/projects/project-2.jpeg";
 export const HERO_CONTENT = `Hi, I’m a Data Analyst with over 2.1 years of experience. I specialize in transforming data into actionable insights to drive business growth. From sales analysis and customer behaviour forecasting to financial reporting. Explore my portfolio to see how I use Power BI, Python, and SQL to solve real-world data challenges.`;
 
 export const ABOUT_TEXT = `
-Hi, I'm a Data Analyst with a background in data analysis, business intelligence, and predictive modelling. With over 2.1 years of experience at Capgemini Engineering, I've worked with industry leaders like Cisco to turn complex datasets into meaningful insights that help drive business decisions.
-I’m passionate about using data to solve real-world problems, from analyzing sales trends to optimizing financial strategies. Skilled in SQL, Python, and Power BI, I create dashboards, build models, and deliver actionable insights.
+Hi, I'm into a Data Technology background where I used data analysis, business analyzing, and forecasting. With over 3+ years of experience at IT Corporte, I've worked with industry leaders like ABB, Cisco, Reliance to turn complex datasets into meaningful insights that help drive business decisions.
+I’m passionate about using data to solve real-world problems, from analyzing products trends to optimizing statistical strategies. Skilled in SQL, Python, and Power BI, I create dashboards, build models, and deliver actionable insights.
 I thrive in collaborative environments, continuously learning and contributing to impactful projects. I’m always excited about making data-driven decisions that drive business growth.
-In my free time, I stay updated on emerging data science trends and contribute to open-source projects. Feel free to explore my portfolio and reach out for collaboration!
+In my free time, I stay updated on emerging AI/ML trends and contribute to open-source projects. Feel free to explore my portfolio and reach out for collaboration!
 `; 
 
 export const EXPERIENCES = [
+  {
+    year: "2025 - Present",
+    role: "Data Analyst",
+    company: "ABB",
+    description: `I leveraged my expertise in data analysis and data engineering to design architectures and build scalable data pipelines. I worked extensively on visualizing complex datasets with over 1 million rows, enabling actionable insights. I collaborated with global leadership teams, helping them use data-driven reports to inform strategic business decisions. As part of an accelerator program focused on business growth, I applied machine learning techniques using Python and Snowflake to generate predictive insights, supporting competitive bidding strategies and identifying opportunities for the next five years, along with other key business perspectives.`,
+    technologies: ["Python", "SQL", "Power BI", "Data Analytics", "Machine Learning", "Forecasting", "Snowflake", "AWS", "Salesforce"],
+  },
+  {
+    year: "June 2024 - Mar 2025",
+    role: "Full Stack Data Science Apprenticeship",
+    company: "AlmaBetter",
+    description:
+      `Completed a 6-month full-stack data science apprenticeship at AlmaBetter, mastering Python, SQL, Machine Learning, and Power BI. Built end-to-end projects focused on data analysis, predictive modeling, and interactive dashboards.`,
+    technologies: ["Python", "PostgreSQL", "Power BI", "Tableau", "Machine Learning"]
+  },
   {
     year: "2021 - 2023",
     role: "Data Analyst",
     company: "Capgemini Engineering",
     description: `Leveraged advanced analytics tools like Python, SQL, and Power BI to analyze and visualize complex datasets, driving actionable insights for Cisco. Collaborated with cross-functional teams to optimize business processes and deliver automated reporting solutions.`,
     technologies: ["Python", "SQL", "Power BI", "Data Analysis", "Tableau", "Machine Learning"],
-  },
-  {
-    year: "Jan 2024 - Present",
-    role: "Full Stack Data Science Apprenticeship",
-    company: "AlmaBetter",
-    description:
-      `Completed a 6-month full-stack data science apprenticeship at AlmaBetter, mastering Python, SQL, Machine Learning, and Power BI. Built end-to-end projects focused on data analysis, predictive modeling, and interactive dashboards.`,
-    technologies: ["Python", "PostgreSQL", "Power BI", "Tableau", "Machine Learning"]
-  }
+  } 
 ];
 
 export const PROJECTS = [
